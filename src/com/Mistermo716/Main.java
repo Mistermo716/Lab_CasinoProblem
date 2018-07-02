6088ec28ce1778174d449a4922a0d1f12597697e
Muaath Alaraj <Muaath.Alaraj@gmail.com>
2018-07-02 10:45:23 -0700

    main class functions

--- src/com/Mistermo716/Main.java
+++ src/com/Mistermo716/Main.java
@@ -1,4 +1,5 @@
 package com.Mistermo716;
+import java.util.Scanner;
 
 public class Main {
 
@@ -12,5 +13,22 @@ public class Main {
         String str = bet1.toString();
 
         System.out.println(str);
+    }
+
+    //validates the bet before giving it to the Triple String class.
+    public static int getBet(int bet){
+        Scanner input = new Scanner(System.in);
+        if(bet >= 0 && bet <=100){
+            return bet;
+        }
+        while(bet < 0 || bet > 100){
+            System.out.println("Please enter a valid bet between 0 & 100");
+            bet = input.nextInt();
+        }
+        return bet;
+    }
+
+    private String randString(){
+        Math.random();
     }
 }
