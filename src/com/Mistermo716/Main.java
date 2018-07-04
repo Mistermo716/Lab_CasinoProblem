ca810334b2d55bfc09ffcf03b2e0f54b9f706d1b
Muaath Alaraj <Muaath.Alaraj@gmail.com>
2018-07-03 17:52:58 -0700

    Complete

--- src/com/Mistermo716/Main.java
+++ src/com/Mistermo716/Main.java
@@ -2,35 +2,49 @@ package com.Mistermo716;
 import java.util.Scanner;
 
 public class Main {
+   public static void main(String[] args) {
+      //creates TripleString object for the game
+      TripleString game = new TripleString();
+      System.out.println("Hello Welcome to the casino");
 
-    public static void main(String[] args) {
-        TripleString game = new TripleString();
-        System.out.println("Hello Welcome to the casino");
+      //for user input
+      Scanner input = new Scanner(System.in);
 
-        Scanner input = new Scanner(System.in);
+      //while loop that will end if user quits or 40 pulls have been made.
+      while(game.numPulls < 40 && game.gameRound == true) {
+            System.out.println(" ");
+            System.out.println("Make a bet between (1-100) or 0 to quit?");
+            int currentInput = 0;
+            //grab next int from user input.
+            currentInput = input.nextInt();
 
-        while(game.numPulls < 40 && game.gameRound == true) {
-            System.out.println("1.  Make a bet and pull");
-            System.out.println("99. Quit");
-            int userInput = input.nextInt();
+            //if input is not 0 we will bet and pull
+            if(currentInput != 0) {
+               int bet = getBet(currentInput);
 
-            if(userInput == 1) {
-                System.out.println("What is your bet?");
-                int currentInput = 0;
-                currentInput = input.nextInt();
-                int bet = getBet(currentInput);
-
-                int multipler = getPayMutiplier(pull(game).toString());
-                game.saveWinnings(bet * multipler);
-                System.out.println("Congrats you win: " + bet*multipler);
+               System.out.println("whirrrrrr .... and your pull is ... ");
+               //multiplier method is called with result of the pull method
+               //it will then convert pull method object return to string
+               int multiplier = getPayMultiplier(pull(game).toString());
+               game.saveWinnings(bet * multiplier);
+               //if the multiplier is greater than 0 they have won.
+               if (multiplier > 0) {
+                  System.out.println("Congrats you win: $" + bet * multiplier);
+               }
+               //if multiplier is equal to zero than lose that round.
+               if (multiplier == 0) {
+                  System.out.println("Sorry you lost this time");
+               }
             }
-            if(userInput == 99){
+            //0 is for quits.
+            if(currentInput == 0){
                 game.setGameRound(false);
                 game.displayWinnings();
             }
 
         }
 
+        //display winnings at the end of the game.
         System.out.println(game.displayWinnings());
 
 
@@ -42,14 +56,19 @@ public class Main {
         if(bet >= 0 && bet <=100){
             return bet;
         }
+        //while the bet is not acceptable loop until it is.
         while(bet < 0 || bet > 100){
             System.out.println("Please enter a valid bet between 0 & 100");
             bet = input.nextInt();
         }
         return bet;
     }
+
+    //returns TripleString Object with the pull
     public static TripleString pull(TripleString objPull){
 
+       //sets the strings of the object
+       //randString() method generates random string
         objPull.setString1(randString());
         objPull.setString2(randString());
         objPull.setString3(randString());
@@ -59,10 +78,13 @@ public class Main {
         return objPull;
     }
 
-    public static int getPayMutiplier(String pulledString){
+    //Will get the multiplier of winnings by the sequence of the strings.
+    public static int getPayMultiplier(String pulledString){
 
+       //will split the string into array indexes by the space in the string
         String arr[] = pulledString.split(" ", 0);
 
+        //will compare sequences for the multiplier
         if(arr[0].equals("cherries") && !arr[1].equals("cherries")){
             return 5;
         }
@@ -79,13 +101,17 @@ public class Main {
             return 100;
         }
 
-        return 1;
+        //if none the money is lost. multiplier of 0
+        return 0;
 
     }
 
+    //will generate random string and return String
     private static String randString(){
+       //math random to generate random number
         int randomNum = (int)(Math.random()* 1000) + 1;
 
+        //each string associated with set of numbers based on probability.
         if(randomNum <= 500){
             return "BAR";
         }
--- src/com/Mistermo716/TripleString.java
+++ src/com/Mistermo716/TripleString.java
@@ -2,14 +2,20 @@ package com.Mistermo716;
 
 public class TripleString {
 
+    //declaring string variables
     private String string1;
     private String string2;
     private String string3;
 
+    //amount of pulls a user is allowed
     public static final int MAX_PULLS = 40;
+    //this will decide if game is ended or not.
     public static boolean gameRound = true;
+    //this will hold an array of winnings which will be added together later.
     private static final int [] pullWinnings = new int [MAX_PULLS];
+    //this will check length of string
     public static final int MAX_LEN = 20;
+    //this is a counter for the number of pulls a user has had.
     public static int numPulls = 0;
 
 
@@ -21,6 +27,8 @@ public class TripleString {
         this.string3 = "";
     }
 
+
+    //class method will check to see if string is valid
     public boolean validString(String str){
         if(!str.equals(null) && str.length() <= this.MAX_LEN){
             return true;
@@ -28,6 +36,7 @@ public class TripleString {
             return false;
     }
 
+    //Below is a set of getters and setters for the string variables
     public String getString1() {
         return string1;
     }
@@ -64,27 +73,32 @@ public class TripleString {
         return false;
     }
 
+
+    //will set whether the game has ended or not.
     public void setGameRound(boolean gameRound){
         this.gameRound = gameRound;
     }
 
+    //will concatenate all strings into one string.
     public String toString(){
         String combinedStr = this.string1 + " " + this.string2 +  " " + this.string3;
         return combinedStr;
     }
 
+    //will save the winnings to an array to keep track of later
     public boolean saveWinnings(int winnings){
         pullWinnings[numPulls] = winnings;
         numPulls += 1;
         return true;
     }
 
+    //will display total winnings to the user.
     public String displayWinnings(){
         int totalWinnings = 0;
         for(int i = 0; i < numPulls; i++){
             totalWinnings += pullWinnings[i];
         }
-        return "Your total Winnings: " + totalWinnings;
+        return "Your total Winnings: $" + totalWinnings;
     }
 
 }
